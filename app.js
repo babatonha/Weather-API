8bee56363661d25e8437f9a2fab20d95841684c9
babatonha <40696889+babatonha@users.noreply.github.com>
2018-11-27 13:46:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var http =  require('http');
+var url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22';
+
+var server = http.createServer(function(request, response){
+    //
+
+    var request = require('request');
+    request(url, function(err, res, body){
+    var data = JSON.parse(body);  
+     response.write("<html><body><div id='conatiner'>");
+     response.write("<h1>"+ 'City Name - :' + data['name'] + '<br>' +"</h1>");
+     response.write("<h2>"+ 'Temperature - :' + data.main['temp'] + '<br>' +"</h2>");
+     response.write("<h2>"+ 'SunSet Time - :' + new Date(data.sys['sunset'] + 1000) + "</h2>");
+     response.write("</div></body></html>");
+     response.end();
+});
+}).listen(8081);
